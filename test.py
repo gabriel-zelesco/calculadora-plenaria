e3510c5e493665fb630f6ee1c67851c81827d4a4
gabriel-zelesco <gabriel.zelesco@contrafluxo.io>
2023-04-16 20:12:27 -0300

    create spec file for buildozer

--- test.py
+++
@@ -1,9 +0,0 @@
-from utils.dhontmethod import DhontMethod
-
-n_seats = 10
-votes = {"chapa1": "210", "chapa2": "345", "chapa3": "415", "chapa4": "100", "chapa5": "555", "chapa6": "123", "abstention": "0"}
-resultados = DhontMethod(votes,n_seats, largest_remainder=True)
-resultados.dhont()
-#resultados.print_report()
-print(resultados.seats)
-print(resultados.text_report())
