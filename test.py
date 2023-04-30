96ba64ad06b750f15e6605d9eaf529dc5cc0282b
gabriel-zelesco <gabriel.zelesco@contrafluxo.io>
2023-04-30 09:29:36 -0300

    major remodeling of the modules

---
+++ test.py
@@ -0,0 +1,5 @@
+from utils.dhontmethod import DhontMethod
+
+teste = DhontMethod()
+print(teste.print_report())
+print(teste.call_list)
--- utils/pollsresult.py
+++ utils/pollsresult.py
@@ -8,42 +8,38 @@ class PollsResult():
     superclass of the classes that will be responsible for the calculations.'''
     
     def __init__(self):
-        self.sort = self.__load_data('options.json').get('sort')
-        self.n_seats = self.__get_n_seats()
-        self.votes = self.__get_votes()
-        self.abstention = self.__get_abstenion()
+        self.data = self.__load_data('data.json')
+        
+        self.sort = self.data['sort']
+        self.abstention_as_valid = self.data['abstention_as_valid']
+        self.n_seats = self.__check_n_seats() 
+        self.votes = self.__get_votes(self.data['votes'])
+        self.abstention = int(self.data['abstention'])
+        self.proportional_limit = self.data['proportional_limit']
+        self.parties = self.__get_parties()
         self.valid_votes = self.__sum_votes()
         self.total_votes = self.valid_votes + self.abstention 
-    
-    def __load_data(self, file_name):
-        with open(file_name, 'r') as f:
-            return json.load(f)
+        self.used_votes = self.__get_used_votes()
         
-    def __str_to_int(self, dict_):
+    
+    def __check_n_seats(self):
+        if int(self.data['n_seats']) < 1:
+            self.data['n_seats'] = "1"
+            self.data['warning_n_seats'] = ['n_seats must be greater than 0.']
+            self.__save_data('data.json', self.data)
+            return 1
+        else:
+            if 'warning_n_seats' in self.data:
+                self.data.pop("warning_n_seats")
+                self.__save_data('data.json', self.data)
+        return int(self.data['n_seats'])
+            
+    
+    def __str_to_int(self,dict_):
         for key in dict_:
             dict_[key] = int(dict_[key])
         return dict_
-    
-    def __get_abstenion(self):
-        abstention = (
-            self
-            .__str_to_int(self.__load_data('abstention.json'))
-            .get('abstention')
-        )
-        return abstention
-    
-    def __save_data(self, file_name, data):
-        with open(file_name, 'w') as f:
-            json.dump(data, f)
-    
-    def __get_n_seats(self):
-        n_seats = self.__str_to_int(self.__load_data('n_seats.json')).get('n_seats')
-        if n_seats < 1:
-            n_seats = 1
-            self.__save_data('n_seats.json', {"n_seats": f'{n_seats}'})
-        return n_seats
-        
-    
+
     def __sort_votes(self, votes):
         if self.sort == 'desc':
             ordered_votes = sorted(
@@ -63,23 +59,50 @@ class PollsResult():
             return votes
         else:
             raise ValueError("The sort parameter must be 'desc' or 'asc'.")
-    
-    def __get_votes(self):
-        votes = self.__str_to_int(self.__load_data('votes.json'))
+
+    def __get_votes(self, votes):
+        votes = self.__str_to_int(votes)
         votes = self.__sort_votes(votes)
         return votes
-
+    
     def __sum_votes(self):
         return sum(self.votes.values())
+    
+    def __get_used_votes(self):
+        if self.abstention_as_valid == "True":
+            return self.total_votes
+        elif self.abstention_as_valid == "False":
+            return self.valid_votes
+        else:
+            raise ValueError("The abstention_as_valid parameter must be \
+                             'True' or 'False' as string.")
 
+    def __load_data(self, file_name):
+        with open(file_name, 'r') as f:
+            return json.load(f)
+        
+    def __save_data(self, file_name, data):
+        with open(file_name, 'w') as f:
+            json.dump(data, f)
+    
+    def __get_parties(self):
+        return list(self.votes.keys())
+                
+        
 
 
 if __name__ == '__main__':
     resultado = PollsResult()
     print(f'votes: {resultado.votes}')
+    print(f'abstention_as_valid: {type(resultado.abstention_as_valid)}')
     print(f'abstention: {resultado.abstention}')
     print(f'valid_votes: {resultado.valid_votes}')
     print(f'total_votes: {resultado.total_votes}')
     print(f'n_seats: {resultado.n_seats}')
     print(f'sort: {resultado.sort}')
+    print(f'used_votes: {resultado.used_votes}')
+    print(f'proportional_limit: {resultado.proportional_limit}')
+    print(f'parties: {resultado.parties}')
+    
+
     
